9287af3402a3ce45a6cc9e1213bffd700e539605
AD <liyoujun600@gmail.com>
2022-02-26 22:03:22 +0800

    ✏️更加詳細的偵測錯誤

--- assets/js/newq.js
+++ assets/js/newq.js
@@ -1,5 +1,5 @@
 var bookid = document.getElementById('bkid').value;//獲取書籍編號
-console.log(bookid)
+console.log("目前書籍編號:"+bookid)
 var el = document.getElementsByName('sheetform');//獲取答案區域
 
 function makeHttpObject() {
@@ -21,7 +21,12 @@ request.onreadystatechange = function() {
                 el[0].innerHTML=request.responseText;//替換答案區域
                 window.open ("https://raw.githubusercontent.com/tooty-1135/easy-happyread-database/main/"+bookid+".txt","解答",height=10,width=10,top=0,left=0)//跳出答案視窗
         } 
-        if (request.status !== 200) {
+        if (request.status == 404) {
+                window.alert("簡單愛閱網:\n這本書尚未被加入答案資料庫")
+                console.log("找不到書籍")
+        }
+        if (request.status !== 404){
                 window.alert("簡單愛閱網:\n發生未知錯誤")
         }
+        console.log("HTTP代碼:"+request.status)
 };
